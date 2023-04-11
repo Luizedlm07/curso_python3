7f13843645afa79a541c012c22eb00a542fd7ccd
luiz.edlm07@gmail.com <luiz.edlm07@gmail.com>
2023-04-10 23:56:59 -0300

    Novos arquivos Django

---
+++ Secao_10/contatos/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.core.paginator import Paginator
+from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+
+# Create your views here.
+def index(request):
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+    
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.order_by('-id').filter(
+        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
+    })
